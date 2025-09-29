25a7c08556dd1acc168d4c7e2a5f01458a475ec9
kizuyoko <kizuyoko@gmail.com>
2025-09-29 13:38:07 +0200

    Header current underline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,26 @@
+"use client";
 import Link from "next/link";
 import { siteConfig } from "@/app/constants/site";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="flex items-center justify-center gap-6">
-      {
-        siteConfig.nav.map(item => (
-          <Link 
+      {siteConfig.nav.map((item) => {
+        const isActive = pathname === item.href;
+        return (
+          <Link
             key={item.name}
-            href={item.href} 
-            className="text-link"
-            aria-label={`Go to ${item.name} page`}>
-              {item.name}
+            href={item.href}
+            className={`text-link ${isActive ? "underline" : ""}`}
+            aria-label={`Go to ${item.name} page`}
+          >
+            {item.name}
           </Link>
-        ))
-      }
+        );
+      })}
     </header>
   );
 }
